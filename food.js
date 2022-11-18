3d20e23faa5c89ce73efca00ca6b971cb6a4ab5f
Marek Cizmar <marekcizmar@outlook.sk>
2022-11-18 15:57:50 +0100

    Base structure of HTML, CSS & JS

---
+++ food.js
@@ -0,0 +1,21 @@
+import {onSnake, expandSnake} from "./snake.js"
+
+let food = {x: 11, y: 0};
+//how much the snakes grow when it eats the food
+const EXPANSION_RATE = 1;
+
+export function update() {
+   if(onSnake(food)){
+    expandSnake(EXPANSION_RATE)
+    food = {x: 20, y: 10};
+   }
+}
+
+export function draw(gameBoard){
+    const foodElement = document.createElement("div")
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add("food")
+    gameBoard.appendChild(foodElement);
+   
+}
